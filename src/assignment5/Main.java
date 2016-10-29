ad834954ca0ee60bc3e24673edcb56440cd575de
kmalyavina <kmalyavina@gmail.com>
2016-10-29 17:25:49 -0500

    Update Main.java

--- src/assignment5/Main.java
+++ src/assignment5/Main.java
@@ -1,15 +1,47 @@
 package assignment5;
+import java.awt.TextField;
+
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	static GridPane grid = new GridPane();
+	
+	GridPane ui = new GridPane(); 		// for aligning ui elements
+	GridPane world = new GridPane(); 	// hold critters
+	
+	private ObservableList critters;
+	
+	// display elements for addCritteer controls
+	Button addCritter = new Button("Add");
+	ComboBox critterAddList = new ComboBox(critters);
+	Label addLabel = new Label("Add Critter");
+	TextField addAmount = new TextField("#");
+	
+	// display elements for timestep controls
+	Label stepLabel = new Label("Time Step");
+	Slider slider = new Slider(1, 100, 1);
+	TextField stepAmount = new TextField();
+	Button step = new Button("Step");
+	
+	// display elements for stats controls
+	Label statsLabel = new Label("Stats");
+	ComboBox critterStatsList = new ComboBox(critters);
+	Button runStats = new Button("Run Stats");
+	Label statsText = new Label("");
+	
+	// quit button
+	Button quit = new Button("Quit");
 
 	@Override
-	public void start(Stage primaryStage) {
-		try {			
+	public void start(Stage primaryStage) throws Exception {
 
 			grid.setGridLinesVisible(true);
 
@@ -21,9 +53,7 @@ public class Main extends Application {
 			// Paints the icons.
 			Painter.paint();
 			
-		} catch(Exception e) {
-			e.printStackTrace();		
-		}
+
 	}
 }
 /*
