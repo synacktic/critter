abf8e9bec49248633404b6fc5e114c8e9cad5e1f
kmalyavina <kmalyavina@gmail.com>
2016-10-30 12:11:16 -0500

    Update Main.java

--- src/assignment5/Main.java
+++ src/assignment5/Main.java
@@ -14,14 +14,16 @@
 package assignment5;
 
 import javafx.application.Application;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
@@ -36,20 +38,22 @@ public class Main extends Application {
 	static GridPane grid = new GridPane();
 	static GridPane controls = new GridPane(); 		// for aligning ui elements
 	
-	private ObservableList<Critter> critters;
-	
+	private ObservableList<Critter> allCritters;
+	private ObservableList<Critter> activeCritters;
+
 	// display elements
 	static Button add = new Button("Add");
 	static Button step = new Button("Step");
 	static Button runStats = new Button("Run Stats");
 	static Button clear = new Button("Clear World");
 	static Button quit = new Button("Quit");
-	private ComboBox<Critter> critterAddList = new ComboBox(critters);
-	private ComboBox<Critter> critterStatsList = new ComboBox(critters);
-	private TextField addAmount = new TextField();
-	private TextField stepAmount = new TextField();
-	private Slider slider = new Slider(1, 100, 1);
+	private ComboBox<Critter> critterAddList = new ComboBox(allCritters);
+	private ComboBox<Critter> critterStatsList = new ComboBox(activeCritters);
+	private IntField addAmount = new IntField(0);
+	private IntField stepAmount = new IntField(1);
+	private Slider slider = new Slider(0, 50, 1);
 	private Text statsText = new Text("");
+	IntegerProperty sliderValue = new SimpleIntegerProperty(1);
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -71,26 +75,31 @@ public class Main extends Application {
 	        	        	        
 	        controls.add(new Label("Add Critter"), 0, 0);
 	        controls.add(critterAddList, 0, 1);
+	        critterAddList.setPromptText("Select a Critter...");
 	        addAmount.setPrefWidth(30);
 	        controls.add(addAmount, 1, 1);
 	       	addAmount.setPromptText("#");
-
-
 	        controls.add(add, 2, 1);
-
+	        
+	        // Step Controls
 	        controls.add(new Label("Time Step"), 0, 2);
 	        controls.add(slider, 0, 3);
+	        slider.setShowTickMarks(true);
+	        slider.setShowTickLabels(true);
 	        controls.add(stepAmount, 1, 3);
 	        stepAmount.setPrefWidth(30);
 	        stepAmount.setPromptText("#");
-
+	        stepAmount.valueProperty().bindBidirectional(slider.valueProperty()); 	// to do: make the textfield editable outside slider shit
 	        controls.add(step, 2, 3);
 	        
+	        // Run Stats Controls
 	        controls.add(new Label("Run Stats"), 0, 4);
 	        controls.add(critterStatsList, 0, 5);
+	        critterStatsList.setPromptText("Select a Critter...");
 	        controls.add(runStats, 1, 5);
 	        controls.add(statsText, 0, 6);
 	        
+	        // Quit & Clear
 	        controls.add(clear, 0, 8);
 	        controls.add(quit, 0, 9);
 	        
