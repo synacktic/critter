141bbdfd435846ee9d7759f69205c4d46b146501
kmalyavina <kmalyavina@gmail.com>
2016-10-30 12:23:09 -0500

    Update Main.java

--- src/assignment5/Main.java
+++ src/assignment5/Main.java
@@ -14,16 +14,12 @@
 package assignment5;
 
 import javafx.application.Application;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
@@ -34,17 +30,17 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	
-	static GridPane ui = new GridPane();
-	static GridPane grid = new GridPane();
-	static GridPane controls = new GridPane(); 		// for aligning ui elements
+	static GridPane ui = new GridPane();			// align controls and world
+	static GridPane grid = new GridPane();			// critter world
+	static GridPane controls = new GridPane(); 		// for aligning control elements
 	
-	private ObservableList<Critter> allCritters;
-	private ObservableList<Critter> activeCritters;
+	private ObservableList<Critter> allCritters;	// list of all critter classes you can make
+	private ObservableList<Critter> activeCritters;	// list of all critter types currently on the board
 
 	// display elements
 	static Button add = new Button("Add");
 	static Button step = new Button("Step");
-	static Button runStats = new Button("Run Stats");
+	static Button runStats = new Button("Stats");
 	static Button clear = new Button("Clear World");
 	static Button quit = new Button("Quit");
 	private ComboBox<Critter> critterAddList = new ComboBox(allCritters);
@@ -53,10 +49,8 @@ public class Main extends Application {
 	private IntField stepAmount = new IntField(1);
 	private Slider slider = new Slider(0, 50, 1);
 	private Text statsText = new Text("");
-	IntegerProperty sliderValue = new SimpleIntegerProperty(1);
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
+	public static void makeSomeCritters() throws Exception{
 		   for (int c=0;c < 25; c++) {        			
 				Critter.makeCritter("Craig");
 				Critter.makeCritter("Critter1");
@@ -69,12 +63,19 @@ public class Main extends Application {
 	    	for (int c=0;c < 100; c++) {        			
 				Critter.makeCritter("Algae");
 			}
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {	//*** Need to take out the "throws Exception" before submitting I think
+			makeSomeCritters();
+	    	
 	        controls.setVgap(10);
 	        controls.setHgap(10);
 	        ui.setPadding(new Insets(10, 10, 30, 10));
 	        	        	        
 	        controls.add(new Label("Add Critter"), 0, 0);
 	        controls.add(critterAddList, 0, 1);
+	        critterAddList.setPrefWidth(150);
 	        critterAddList.setPromptText("Select a Critter...");
 	        addAmount.setPrefWidth(30);
 	        controls.add(addAmount, 1, 1);
@@ -84,6 +85,7 @@ public class Main extends Application {
 	        // Step Controls
 	        controls.add(new Label("Time Step"), 0, 2);
 	        controls.add(slider, 0, 3);
+	        slider.setPrefWidth(150);
 	        slider.setShowTickMarks(true);
 	        slider.setShowTickLabels(true);
 	        controls.add(stepAmount, 1, 3);
@@ -95,6 +97,7 @@ public class Main extends Application {
 	        // Run Stats Controls
 	        controls.add(new Label("Run Stats"), 0, 4);
 	        controls.add(critterStatsList, 0, 5);
+	        critterStatsList.setPrefWidth(150);
 	        critterStatsList.setPromptText("Select a Critter...");
 	        controls.add(runStats, 1, 5);
 	        controls.add(statsText, 0, 6);
