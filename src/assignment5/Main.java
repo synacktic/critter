2d3b2ec201f545336cab4842ddc2547b85ce33b2
kmalyavina <kmalyavina@gmail.com>
2016-11-05 11:45:12 -0500

    Added Seed Section

--- src/assignment5/Main.java
+++ src/assignment5/Main.java
@@ -16,7 +16,6 @@ package assignment5;
 import java.lang.reflect.Method;
 import java.lang.reflect.InvocationTargetException;
 import java.util.NoSuchElementException;
-import java.util.Set;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -30,9 +29,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -57,12 +59,15 @@ public class Main extends Application {
 	static Button clear = new Button("Clear World");
 	static Button quit = new Button("Quit");
 	static Button animate = new Button("Animate Steps");
+	static Button setSeed = new Button("Seed");
 	private ComboBox<Critter> critterAddList = new ComboBox(allCritters);
 	private ComboBox<Critter> critterStatsList = new ComboBox(activeCritters);
+	private TextField seedAmount = new TextField();
 	private IntField addAmount = new IntField(0);
 	private IntField stepAmount = new IntField(1);
 	private Slider slider = new Slider(0, 50, 1);
 	private Text statsText = new Text("");
+	private Text seed = new Text("No seed set");
 	private Integer stepnumber = 0;
 	private Label stepNum = new Label("Step: " + stepnumber);
 
@@ -80,7 +85,7 @@ public class Main extends Application {
 				Critter.makeCritter("Algae");
 			}
 	}
-
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {	//*** Need to take out the "throws Exception" before submitting I think
 			makeSomeCritters();
@@ -96,14 +101,22 @@ public class Main extends Application {
 	        ui.add(controls, 0, 0);
 	        ui.add(grid, 1, 0);
 	        
+	        
+	        // Set seed controls
+	        controls.add(new Label("Set Seed"), 0, 0);
+	        controls.add(seedAmount, 0, 1);
+	        controls.add(setSeed, 1, 1);
+	        controls.add(seed, 0, 2);
+	        seed.setFill(Color.GRAY);	        
+	        
 	        // Add Critter Controls
-	        controls.add(new Label("Add Critter"), 0, 0);			// title lable
-	        controls.add(critterAddList, 0, 1);						// combobox
+	        controls.add(new Label("Add Critter"), 0, 3);			// title lable
+	        controls.add(critterAddList, 0, 4);						// combobox
 	        critterAddList.setPrefWidth(150);						
 	        critterAddList.setPromptText("Select a Critter...");	// prompt
 	        addAmount.setPrefWidth(30);								
-	        controls.add(addAmount, 1, 1);							// input field
-	        controls.add(add, 2, 1);								// button
+	        controls.add(addAmount, 1, 4);							// input field
+	        controls.add(add, 2, 4);								// button
 	        
 		       add.setOnAction(new EventHandler<ActionEvent>() {  		    	   
 		            @Override
@@ -121,17 +134,17 @@ public class Main extends Application {
 		        });	
 		       
 	        // Step Controls
-	        controls.add(new Label("Time Step"), 0, 3);				// title lable
-	        controls.add(slider, 0, 4);								// slider
+	        controls.add(new Label("Time Step"), 0, 6);				// title label
+	        controls.add(slider, 0, 7);								// slider
 	        slider.setPrefWidth(150);								
 	        slider.setShowTickMarks(true);							
 	        slider.setShowTickLabels(true);							
-	        controls.add(stepAmount, 1, 4);							// input field
+	        controls.add(stepAmount, 1, 7);							// input field
 	        stepAmount.setPrefWidth(30);							
-	        stepAmount.valueProperty().bind(slider.valueProperty()); // bind amount box to slider
-	        controls.add(step, 2, 4);								// button
-	        controls.add(animate, 0, 5);
-	        controls.add(stepNum, 2, 3);
+	        stepAmount.valueProperty().bindBidirectional(slider.valueProperty()); // bind amount box to slider
+	        controls.add(step, 2, 7);								// button
+	        controls.add(animate, 0, 8);
+	        controls.add(stepNum, 2, 6);
 
 		       step.setOnAction(new EventHandler<ActionEvent>() {  		    	   
 		            @Override
@@ -178,11 +191,11 @@ public class Main extends Application {
 //		         	(updated whenever the view is updated)? 
 //		       - Can the user select which critter class(es) have their runStats methods updated? 
 //		       - By default is theCritter.runStats base class method invoked each time the view is updated?
-	        controls.add(new Label("Run Stats"), 0, 7);				// title lable
-	        controls.add(critterStatsList, 0, 8);					// combobox
+	        controls.add(new Label("Run Stats"), 0, 10);				// title lable
+	        controls.add(critterStatsList, 0, 11);					// combobox
 	        critterStatsList.setPrefWidth(150);				
 	        critterStatsList.setPromptText("Select a Critter...");	// prompt
-	        controls.add(statsText, 0, 7);							// textbox
+	        controls.add(statsText, 0, 12);							// textbox
 	        // sample output in statsText
 		        /*
 		         * CritterName1
@@ -191,7 +204,7 @@ public class Main extends Application {
 		         * CritterName2
 		         * [CritterName2 runStats output]
 		         */
-	        controls.add(runStats, 1, 8);							// button
+	        controls.add(runStats, 1, 11);							// button
 
 		       runStats.setOnAction(new EventHandler<ActionEvent>() {  		    	   
 		            @Override
@@ -212,7 +225,7 @@ public class Main extends Application {
 		        });	
 		       
 	        // Clear
-	        controls.add(clear, 0, 11);								// button
+	        controls.add(clear, 0, 14);								// button
 	        
 		       clear.setOnAction(new EventHandler<ActionEvent>() {  		    	   
 		            @Override
@@ -223,7 +236,7 @@ public class Main extends Application {
 		        });		
 		     
 		    // Quit   
-	        controls.add(quit, 0, 12);								// button
+	        controls.add(quit, 0, 15);								// button
 	        
 		       quit.setOnAction(new EventHandler<ActionEvent>() {   	   
 		            @Override
@@ -236,7 +249,7 @@ public class Main extends Application {
 			
 	        Critter.displayWorld();
 
-			Scene scene = new Scene(ui, 710, 410);		
+			Scene scene = new Scene(ui, 750, 550);		
  
 	        primaryStage.setScene(scene);
 	        primaryStage.show();
